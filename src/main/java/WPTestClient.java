00a9894469ec7a16b1293908990aecfa97d34db8
Angela Korra'ti <annathepiper@fastmail.com>
2019-01-21 14:20:18 -0800

    Initial commits for WP Test Demo project, POM file, first stubs for Client class, IML file

---
+++ src/main/java/WPTestClient.java
@@ -0,0 +1,47 @@
+import org.json.JSONArray;
+
+/**
+ * WPTestClient
+ * @author Angela Korra'ti
+ *
+ * This class is the helper client the tests will use to interact with the Wordpress API endpoints. It uses Unirest
+ * to do the actual GET, POST, etc. calls.
+ */
+public class WPTestClient {
+
+    /**
+     * getPosts
+     * Get a list of posts on the test Wordpress site. (GET request)
+     * @return
+     */
+    public void getPosts() {
+    }
+
+    /**
+     * createPost
+     * Create a post and send it to the Wordpress site. (POST request)
+     */
+    public void createPost() {
+    }
+
+    /**
+     * getPost
+     * Get a specific post on the test Wordpress site by post ID. (GET request)
+     */
+    public void getPost() {
+    }
+
+    /**
+     * updatePost
+     * Update a specific post on the test Wordpress site by post ID. (POST request)
+     */
+    public void updatePost() {
+    }
+
+    /**
+     * deletePost
+     * Delete a specific post on the test Wordpress site by post ID. (DELETE request)
+     */
+    public void deletePost() {
+    }
+}
