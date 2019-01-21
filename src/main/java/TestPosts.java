75b3ef0954d9d2d45af0be269afba0f7cfc83315
Angela Korra'ti <annathepiper@fastmail.com>
2019-01-21 15:44:00 -0800

    Work in progress on the TestPosts test case

--- src/main/java/TestPosts.java
+++ src/main/java/TestPosts.java
@@ -1,4 +1,18 @@
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONArray;
+import org.testng.annotations.Test;;
 
-public class TestPosts {
-    public void TestGetPosts() {}
+/**
+ * TestPosts
+ * @author Angela Korra'ti
+ *
+ * Last updated 1/21/2019
+ * This class contains test cases related to posts on the Wordpress test site.
+ */
+public class TestPosts extends BaseTest {
+
+    @Test
+    public void TestGetPosts() throws UnirestException {
+        wpTC.getPosts();
+    }
 }
