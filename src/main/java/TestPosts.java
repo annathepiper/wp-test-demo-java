fd66bd2513a46c45349e503e00700d46d3db3ecb
Angela Korra'ti <annathepiper@fastmail.com>
2019-01-22 16:16:34 -0800

    Adding second test case to get a post by the specified ID in the properties file

--- src/main/java/TestPosts.java
+++ src/main/java/TestPosts.java
@@ -1,5 +1,6 @@
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -7,7 +8,7 @@ import org.testng.annotations.Test;
  * TestPosts
  * @author Angela Korra'ti
  *
- * Last updated 1/21/2019
+ * Last updated 1/22/2019
  * This class contains test cases related to posts on the Wordpress test site.
  */
 public class TestPosts extends BaseTest {
@@ -21,5 +22,23 @@ public class TestPosts extends BaseTest {
     public void TestGetPostsReturnsPosts() throws UnirestException {
         JSONArray response = wpTC.getPosts();
         Assert.assertTrue(response.length() > 0, "GetPosts endpoint not returning at least one object in JSONArray.");
+        Assert.assertNotNull(response,"GetPosts endpoint returned a null response.");
+    }
+
+    /**
+     * TestGetPostById
+     * Verify that you can get a post by a specific ID off the GetPosts endpoint. Uses a test ID set in the
+     * properties file and retrieved by the BaseTest class.
+     * @throws UnirestException
+     */
+    @Test
+    public void TestGetPostById() throws UnirestException {
+        JSONObject response = wpTC.getPost(getPostId);
+        JSONObject renderedTitle = response.getJSONObject("title");
+        Assert.assertNotNull(response,"GetPosts endpoint returned a null object. Post may not exist.");
+        Assert.assertEquals(response.get("id").toString(), getPostId,
+                "GetPosts endpoint didn't return correct ID number.");
+        Assert.assertEquals(renderedTitle.get("rendered"),"Hello, readers!",
+                "Retrieved post from GetPosts endpoint does not have expected title.");
     }
 }
