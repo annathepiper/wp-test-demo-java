6215f08c62de87e37cda62c84e6f2e5e4f3a320b
Angela Korra'ti <annathepiper@fastmail.com>
2019-01-22 16:16:04 -0800

    Adding support for getPostId value added to the properties file

--- src/main/java/BaseTest.java
+++ src/main/java/BaseTest.java
@@ -5,12 +5,13 @@ import java.util.ResourceBundle;
  * BaseTest
  * @author Angela Korra'ti
  *
- * Last updated 1/21/2019
+ * Last updated 1/22/2019
  * This test class is the master class for the TestNG suite. Does necessary setup work for all classes.
  */
 public class BaseTest {
     public String protocol;
     public String host;
+    public String getPostId;
     private static ResourceBundle rb = ResourceBundle.getBundle("wp-test-demo");
     public WPTestClient wpTC;
 
@@ -19,8 +20,9 @@ public class BaseTest {
         // Get the needed properties out of the file
         protocol = rb.getString("protocol");
         host = rb.getString("host");
+        getPostId = rb.getString("getPostId");
 
         // We also need a WPTestClient object for all the tests to use
-        wpTC = new WPTestClient(protocol, host);
+        wpTC = new WPTestClient(protocol, host, getPostId);
     }
 }
